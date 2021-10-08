c2ffa2277d1e45ab8441ab193aa69cc61934b713
DugDaniels <cdouglasd@gmail.com>
2021-10-08 13:25:34 -0400

    Test for adding a payee

--- test/Payouts.js
+++ test/Payouts.js
@@ -12,23 +12,31 @@ describe("Payouts contract", () => {
 
   beforeEach(async () => {
     Payouts = await ethers.getContractFactory("Payouts");
-    [acct1] = await ethers.getSigners();
+    [acct1, acct2] = await ethers.getSigners();
     payouts = await Payouts.deploy();
   }) 
 
   describe("Deployment", () => {
-    it("Should set payouts as inactive", async () => {
+    it("Should initialize payouts as inactive", async () => {
       expect(await payouts.activated(acct1.address)).to.equal(false);
     });
     
-    it("Should have payouts with no payees", async () => {
+    it("Should initialize payouts with no payees", async () => {
       const payeeList = await payouts.getPayees();
       expect(Array.isArray(payeeList)).to.equal(true);
       expect(payeeList.length).to.equal(0);
     });
 
-    it("Should set balances to zero", async () => {
+    it("Should initialize balances at zero", async () => {
       expect(await payouts.getBalance()).to.equal(0);
+    });
+  });
+
+  describe("Adding payees", () => {
+    it("Should allow a payee to be added", async () => {
+      await payouts.addPayee(acct2.address, 1);
+      const payeeList = await payouts.getPayees();
+      expect(payeeList[0]).to.equal(acct2.address);
     });
   });
 });
