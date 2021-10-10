7296bed8d9bb9cf7955acbdcb3721729db9fbcf4
DugDaniels <cdouglasd@gmail.com>
2021-10-10 17:45:27 -0400

    Clean up default CRA layout/styles

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import { useEthereum } from "./providers/EthereumProvider";
 import { usePayouts } from "./providers/PayoutsProvider";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const { address } = useEthereum();
   const payouts = usePayouts();
-  console.log("Address:", address);
-  console.log(payouts.address);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {address ? 
+        <>
+          <p>Wallet address: {address}</p>
+          <p>Contract address: {payouts.address}</p>
+        </>
+      : null}
     </div>
   );
 }
