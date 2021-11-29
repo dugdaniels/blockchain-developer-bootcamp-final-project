5d94299b2fd66b68378f7b61644ad8fa37ab8d3c
DugDaniels <cdouglasd@gmail.com>
2021-11-28 19:03:13 -0500

    Add comments

--- contracts/Payouts.sol
+++ contracts/Payouts.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
+/// @title Payouts
+/// @author @DugDaniels
+/// @notice You can use this public contract to split payments to multiple recipients
+/// @dev Any address can configure a split and distribute funds to multiple recipients
 contract Payouts {    
 
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -20,6 +24,8 @@ contract Payouts {
     mapping(address => uint) totalAllocations;
     mapping(address => mapping(address => uint)) splits;
 
+    /// @notice Activates a payout if it has at least one payee, otheswirs it will be deactivated
+    /// @param _payoutsAddress The address of the payout owner
     function _updateActivated(address _payoutsAddress) internal {
         if (EnumerableSet.length(payees[_payoutsAddress]) == 0 && activated[_payoutsAddress]) {
             activated[_payoutsAddress] = false;
@@ -28,10 +34,15 @@ contract Payouts {
         }
     }
 
+    /// @notice Checks if the callers payout is active
+    /// @return True if active, false otherwise
     function isActivated() public view returns (bool) {
         return activated[msg.sender];
     }
 
+    /// @notice Adds a payee to the split
+    /// @param _payeeAddress The address of a split recipient
+    /// @param _payeeSplit The weight of the recipients split
     function addPayee(address _payeeAddress, uint _payeeSplit) public {
         require(splits[msg.sender][_payeeAddress] == 0, "Account already added as a payee");
         require(_payeeAddress != address(0), "Can't add the zero address as a payee");
@@ -44,6 +55,8 @@ contract Payouts {
         _updateActivated(msg.sender);
     }
 
+    /// @notice Removes a payee from the split
+    /// @param _payeeAddress The address of an existing split recipient
     function removePayee(address _payeeAddress) public {
         require(splits[msg.sender][_payeeAddress] > 0, "Account not listed as a payee");
         EnumerableSet.AddressSet storage payeeList = payees[msg.sender];
@@ -55,6 +68,10 @@ contract Payouts {
         _updateActivated(msg.sender);
     }
 
+    /// @notice Edits an existing payee and their split
+    /// @param _originalPayeeAddress The address of an existing split recipient
+    /// @param _newPayeeAddress The address to update the payee to, may be the same as the original
+    /// @param _payeeSplit The updated weight of the recipients split, may be the same as the original
     function editPayee(address _originalPayeeAddress, address _newPayeeAddress, uint _payeeSplit) public {
         if (_originalPayeeAddress != _newPayeeAddress) {
             removePayee(_originalPayeeAddress);
@@ -72,6 +89,8 @@ contract Payouts {
         }
     }
 
+    /// @notice Returns a list of all payees the caller has configured
+    /// @return An array of Payee objects containing addresses and split weights
     function getPayees() public view returns (Payee[] memory) {
         uint payeeCount = EnumerableSet.length(payees[msg.sender]);
         Payee[] memory payeeList = new Payee[](payeeCount);
@@ -85,14 +104,21 @@ contract Payouts {
         return payeeList;
     }
 
+    /// @notice Get the weight of a payees split
+    /// @param _payeeAddress The address of an existing split recipient
+    /// @return The weight of the payees split as a uint
     function getPayeeSplit(address _payeeAddress) public view returns (uint) {
         return splits[msg.sender][_payeeAddress];
     }
 
+    /// @notice Gets the callers balance
+    /// @return The callers balance as a uint
     function getBalance() public view returns (uint) {
         return balances[msg.sender];
     }
 
+    /// @notice Internal function that splits a payment to multiple payees by weight and updates the balances
+    /// @param _paymentValue The payment value to split
     function routePayment(uint _paymentValue) internal {
         if (_paymentValue < EnumerableSet.length(payees[msg.sender])) {
             balances[msg.sender] += _paymentValue;
@@ -108,6 +134,7 @@ contract Payouts {
         }
     }
 
+    /// @notice Allows the caller to withdraw their balance
     function withdraw() public {
         require(balances[msg.sender] > 0, "You have no funds to withdraw");
         uint balance = balances[msg.sender];
@@ -116,6 +143,7 @@ contract Payouts {
         require(sent, "Failed to withdraw Ether");
     }
 
+    /// @notice Payable funtion that allows the caller to initeates a split payment
     receive() external payable {
         require(activated[msg.sender], "This payout is not active");
         if (msg.value > 0) {
